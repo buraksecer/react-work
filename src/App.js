f32518aea7e0cb7156d122985376e5036c2d96c3
buraksecer <burak.secer@deliveryhero.com>
2022-09-26 09:25:23 +0300

    day2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Articles } from './pages/Articles';
+import { CounterButton } from "./CounterButton";
+import {CounterProvider} from "./CounterProvider";
 
 const BigGreenHeading = styled.h1`
 	color: green;
@@ -13,29 +15,34 @@ const BigGreenHeading = styled.h1`
 const App = () => {
 	return (
 		<>
-		<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
-		<ul>
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/about">About</Link>
-			</li>
-			<li>
-				<Link to="/articles">Articles</Link>
-			</li>
-		</ul>
-		<Switch>
-			<Route path="/" exact>
-				<Home />
-			</Route>
-			<Route path="/about">
-				<About />
-			</Route>
-			<Route path="/articles">
-				<Articles />
-			</Route>
-		</Switch>
+			<CounterProvider>
+				<CounterButton/>
+
+				<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/articles">Articles</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/articles">
+						<Articles />
+					</Route>
+				</Switch>
+			</CounterProvider>
+
 		</>
 	);
 }
